77fac24af079517c78512d9e76a1ae39735a1edc
brandon <brandon.ngongolo@student.moringaschool.com>
2023-02-27 21:38:31 -0500

    Complete

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors.full_name.each do |actor|
+        "#{actor.&:full_name} - #{actor.show}"
+    end
+  end
+
+
+
+
+
   
 end
